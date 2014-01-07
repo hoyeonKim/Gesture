8fd2415c0741823327e03f3bca3c0be93e1bce8b
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-07 22:00:36 +0900

    Gesture
    complete

--- p244/ViewController.m
+++ p244/ViewController.m
@@ -9,15 +9,32 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *ballImage;
 
 @end
 
 @implementation ViewController
 
+-(IBAction)handleRotate:(UIRotationGestureRecognizer *)gesture{
+    self.ballImage.transform = CGAffineTransformRotate(self.ballImage.transform, gesture.rotation);
+    [gesture setRotation:0.0];
+}
+-(void)handlePan:(UIPanGestureRecognizer *)gesture{
+    CGPoint point = [gesture locationInView:self.view];
+    self.ballImage.center = point;
+}
+
+-(void)handlePinch:(UIPinchGestureRecognizer *)gesture{
+    CGFloat scale = gesture.scale;
+    self.ballImage.transform = CGAffineTransformScale(self.ballImage.transform, scale, scale);
+    [gesture setScale:1.0];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIRotationGestureRecognizer *rotate = [[UIRotationGestureRecognizer alloc]initWithTarget:self action:@selector(handleRotate:)];
+    [self.ballImage addGestureRecognizer:rotate];
 }
 
 - (void)didReceiveMemoryWarning
